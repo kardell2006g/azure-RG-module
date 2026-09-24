c54a82b14868768a59a99a36667160c93fd6e9a2
Adam K <50672995+kardell2006g@users.noreply.github.com>
2026-09-24 15:17:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,38 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.43.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
     }
   }
 }
+
 provider "azurerm" {
   features {}
 }
 
-resource "random_integer" "suffix" {
-  min = 10000
-  max = 99999
+variable "rg_count" {
+  description = "Number of resource groups to create"
+  type        = number
+  default     = 3
 }
 
+resource "random_integer" "suffix" {
+  count = var.rg_count
+  min   = 10000
+  max   = 99999
+}
 
 resource "azurerm_resource_group" "RUSH_RG" {
-  name = "rush${random_integer.suffix.result}"
+  count    = var.rg_count
+  name     = "rush${random_integer.suffix[count.index].result}"
   location = "eastus"
-  count = var.rg_count
-tags = {
+  tags = {
     costcenter = "ABC-12345"
-    iteration = "Resource Group number ${count.index}"
-     }
+    iteration  = "Resource Group number ${count.index}"
+  }
 }
